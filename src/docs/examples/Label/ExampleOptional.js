a794c956531909a5f13ef163ba76e99d38c9f1f6
Igor Octaviano <igoroctaviano@gmail.com>
2018-11-27 18:57:42 -0200

    Create Label component and examples

---
+++ src/docs/examples/Label/ExampleOptional.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Label from 'ps-react-igoroctaviano/Label';
+
+/** Optional label */
+export default function OptionalLabel() {
+  return <Label htmlFor="test" label="How are you?!" />;
+}
