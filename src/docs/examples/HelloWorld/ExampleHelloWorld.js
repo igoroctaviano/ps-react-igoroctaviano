7cf03c9583b6b88ff87e35893bf0c78f1d3a8e45
Igor Octaviano <igoroctaviano@gmail.com>
2018-11-27 15:07:32 -0200

    Short path

--- src/docs/examples/HelloWorld/ExampleHelloWorld.js
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HelloWorld from 'ps-react-igoroctaviano/HelloWorld/HelloWorld';
+import HelloWorld from 'ps-react-igoroctaviano/HelloWorld';
 
 export default function ExampleHelloWorld() {
   return <HelloWorld message="=) igoroctaviano viewers!" />
